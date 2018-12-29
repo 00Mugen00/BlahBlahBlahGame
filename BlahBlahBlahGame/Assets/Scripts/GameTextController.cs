f5b9c28a0c4d1527749b86d6d1f954734b4027f4
00Mugen00 <genericblue36@gmail.com>
2018-12-29 21:41:43 +0100

    GameTextController and State Scriptable Object implemented
    I also a dded a Jojo Reference (just in case)

---
+++ BlahBlahBlahGame/Assets/Scripts/GameTextController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTextController : MonoBehaviour {
+
+    [SerializeField]
+    Text gameText;
+    [SerializeField]
+    State startingState;
+
+    State currentState;
+
+	void Start () {
+        currentState = startingState;
+        gameText.text = currentState.GetStateStory();
+	}
+	
+	void Update () {
+        State[] nextStates = currentState.GetNextState();
+        for(int i=0; i<nextStates.Length; i++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                currentState = nextStates[i];
+            }
+        }
+        gameText.text = currentState.GetStateStory();
+	}
+}
---
+++ BlahBlahBlahGame/Assets/Scripts/State.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : ScriptableObject {
+
+    [TextArea(10, 10)]
+    [SerializeField]
+    string storyText;
+    [SerializeField]
+    State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextState()
+    {
+        return nextStates;
+    }
+
+}
